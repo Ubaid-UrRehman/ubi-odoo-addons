0cde87f996cfaafac4846cf4c26ee1f8339db817
Ubaid ur Rehman <ubaidur.rehman018@yahoo.com>
2018-03-22 00:28:11 +0300

    Changed field type and remove print statements.

--- customer_code/models/models.py
+++ customer_code/models/models.py
@@ -6,17 +6,14 @@ from odoo.exceptions import ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    unique_id = fields.Integer(string='Customer code', required=False, help="The Unique Sequence no")
+    unique_id = fields.Char(string='Customer code', required=False, help="The Unique Sequence no")
 
     @api.constrains('unique_id')
     def _check_unique_id(self):
         for record in self:
             if record.unique_id:
-                print record.unique_id
-                print str(record.unique_id)
-                print len(str(record.unique_id))
-                if len(str(record.unique_id)) > 10 or len(str(record.unique_id)) < 10:
-                    raise ValidationError("Code must be 10 digits long")
+                if len(record.unique_id) > 25:
+                    raise ValidationError("More than 25 characters are not allowed")
 
     _sql_constraints = [
         ('unique_id_unique',
